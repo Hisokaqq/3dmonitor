fd6f39854583f375ec2b0b3d655ff56cfdacd5e3
Alex <baklan4ik1993@gmail.com>
2023-06-11 15:40:07 -0700

    3d mac

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
-
+import { Canvas } from '@react-three/fiber';
+import { Mac } from './components/Mac';
+import { OrbitControls, PresentationControls } from '@react-three/drei';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Canvas camera={{fov: 45, near: .1, far: 2000, position: [-3, 1.5, 4]}}>
+      <ambientLight intensity={1} />
+      <PresentationControls polar={[-.4, .2]}>
+        
+       <Mac position-y={-1.2} />
+       </PresentationControls>
+    </Canvas>
   );
 }
 
